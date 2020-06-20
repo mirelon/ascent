cc0c0e0645917627512b28a528eec159254b080a
Michal Kovac <miso@github.ksp.sk>
2020-06-20 10:24:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function update() {
   let a = new Date() - startOfDay
   let b = Math.max(a-21600000, 0)
   let full = b / 1800000 + 36 * ((startOfDay - Date.parse('2020-05-18T00:00:00+0200')) / 86400000 - 30)
-  document.getElementsByClassName('project-name')[0].textContent = full.toFixed(2) + ' m'
+  document.getElementsByClassName('project-name')[0].textContent = full.toFixed(4) + ' m'
 }
 
 setInterval(update, 50)
