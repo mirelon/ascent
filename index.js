0bec5cf491845d2660414abfff0f927d5ff5b879
mirelon <miso@github.ksp.sk>
2024-05-29 08:03:50 +0200

    Add more birthdays

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ const updateElements = (className, text) => {
 
 function update() {
   const now = new Date()
-  const olivkaBirthDate = Date.parse('2021-02-24T00:00:00+0100')
-  const sarlotkaBirthDate = Date.parse('2023-09-12T00:00:00+0200')
-  const olivkaDays = Math.floor((now - olivkaBirthDate) / DAY_MS)
-  const sarlotkaDays = Math.floor((now - sarlotkaBirthDate) / DAY_MS)
-  const olivkaAkoSarlotka = olivkaBirthDate + sarlotkaDays * DAY_MS
-  updateElements('olivka', olivkaDays)
-  updateElements('sarlotka', sarlotkaDays)
+  const birthDatesRaw = {
+    'ja': '1988-05-21T00:00:00+0200',
+    'livka': '1992-10-15T00:00:00+0200',
+    'olivka': '2021-02-24T00:00:00+0100',
+    'sarlotka': '2023-09-12T00:00:00+0200',
+  }
+  const birthDates = Object.fromEntries(Object.entries(birthDatesRaw).map(([el, birthDateRaw]) => [el, Date.parse(birthDateRaw)]))
+  const agesInDays = Object.fromEntries(Object.entries(birthDates).map(([el, birthDate]) => [el, Math.floor((now - birthDate) / DAY_MS)]))
+  Object.entries(agesInDays).forEach(([el, ageInDays]) =>
+    updateElements(el, ageInDays),
+  )
+  const olivkaAkoSarlotka = birthDates['olivka'] + agesInDays['sarlotka'] * DAY_MS
   updateElements('olivka_ako_sarlotka', new Date(olivkaAkoSarlotka).toDateString())
 }
 
